085d814e96c3a846ca59e0505112b4d0821915eb
Adnan <aaarras@ibadia.cat>
2022-04-26 20:45:35 +0200

    Pruebas con el mando

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 import * as THREE from 'three'
-console.log(Controller.search())
+console.log(await Controller.search())
+
+
+//Funcion que detecta un mando y lo almacena en una variable
+window.addEventListener('gc.controller.found', function() {
+    var controllerPS4 = Controller.getController(0);
+    window.a = controllerPS4.settings.list();
+}, false);
+
+
+// Funcion que detecta los botones del mando (Cruzeta,gatillos,botones)
+window.addEventListener('gc.button.press', function(event) {
+    var button = event.detail;
+    console.log(button);
+    buttonPressed(event)
+
+}, false);
+
+// Funcion que detecta los joysticks
+window.addEventListener('gc.analog.start', function(event) {
+    var stick = event.detail;
+    console.log(stick);
+}, false);
+
+
 
