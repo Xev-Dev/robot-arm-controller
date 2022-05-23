45144eea50e64ad1c30ff04b5afa8ec9c4129b6d
Adnan <aaarras@ibadia.cat>
2022-05-23 16:30:11 +0100

    server config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite"
 export default defineConfig({
     server: {
-      host: true
-    }
+      port: 3001,
+      host: "0.0.0.0",
+      hmr: {
+        clientPort: " ",
+      },
+    },
 })
