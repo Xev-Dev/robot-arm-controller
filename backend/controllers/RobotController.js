e21293fb35296f607ecb349c482b78dd6d2dd0b3
xev023 <xamobarbaran@gmail.com>
2022-05-30 20:52:04 +0200

    starting robot controller backend

--- backend/controllers/RobotController.js
+++ backend/controllers/RobotController.js
@@ -5,7 +5,11 @@ router.post('/registerMove/:idRecord',async(req,res)=>{
 
 })
 router.post('/registerPosition/:idRecord',async(req,res)=>{
-
+    const arm1 = req.body.arm1
+    const arm2 = req.body.arm2
+    const arm3 = req.body.arm3
+    const base = req.body.base
+    const head = req.body.head
 })
 router.post('/registerRecord/:idUser',async(req,res)=>{
 
@@ -14,12 +18,7 @@ router.get('/getMove/:idRecord',async(req,res)=>{
 
 })
 router.get('/getPosition/:idRecord',async(req,res)=>{
-    const arm1 = req.body.arm1
-    const arm2 = req.body.arm2
-    const arm3 = req.body.arm3
-    const base = req.body.base
-    const head = req.body.head
-    
+    connection.query(`SELECT * FROM position WHERE ID_Grabacion =`)
 })
 router.get('/getRecord/:idUser',async(req,res)=>{
 
