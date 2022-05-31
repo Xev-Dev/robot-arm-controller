990fb134ea4b79b42ed6b97eccbe4ceebd7bd351
Adnan <aaarras@ibadia.cat>
2022-05-31 17:30:38 +0200

    grabacion funcion

--- src/controllerInputs.js
+++ src/controllerInputs.js
@@ -12,21 +12,45 @@ window.addEventListener('gc.button.hold', function (event) {
     switch (button.name) {
         case "LEFT_SHOULDER":
             window.guis.armBase3.setValue(window.guis.armBase3.object._z + 0.05)
+            if (window.record) {
+                window.change = ["armbase3", 0.05]
+                window.movement.push(window.change)
+            }
             break
         case "RIGHT_SHOULDER":
             window.guis.armBase3.setValue(window.guis.armBase3.object._z - 0.05)
+            if (window.record) {
+                window.change = ["armbase3", -0.05]
+                window.movement.push(window.change)
+            }
             break
         case "LEFT_SHOULDER_BOTTOM":
             window.guis.armBase4.setValue(window.guis.armBase4.object._z + 0.05)
+            if (window.record) {
+                window.change = ["armBase4", 0.05]
+                window.movement.push(window.change)
+            }
             break
         case "RIGHT_SHOULDER_BOTTOM":
             window.guis.armBase4.setValue(window.guis.armBase4.object._z - 0.05)
+            if (window.record) {
+                window.change = ["armBase4", -0.05]
+                window.movement.push(window.change)
+            }
             break
         case "DPAD_LEFT":
             window.guis.armBase5.setValue(window.guis.armBase5.object._z + 0.05)
+            if (window.record) {
+                window.change = ["armBase5", 0.05]
+                window.movement.push(window.change)
+            }
             break
         case "DPAD_RIGHT":
             window.guis.armBase5.setValue(window.guis.armBase5.object._z - 0.05)
+            if (window.record) {
+                window.change = ["armBase5", -0.05]
+                window.movement.push(window.change)
+            }
             break
         default:
             break
@@ -40,15 +64,31 @@ window.addEventListener('gc.analog.hold', function (event) {
         case "LEFT_ANALOG_STICK":
             if (stick.position.x < 0) {
                 window.guis.armBase2.setValue(window.guis.armBase2.object._y + (stick.position.x * -1) * 0.2)
+                if (window.record) {
+                    window.change = ["armBase2", (stick.position.x * -1) * 0.2]
+                    window.movement.push(window.change)
+                }
             } else {
                 window.guis.armBase2.setValue(window.guis.armBase2.object._y - (stick.position.x) * 0.2)
+                if (window.record) {
+                    window.change = ["armBase2", -Math.abs((stick.position.x * -1) * 0.2)]
+                    window.movement.push(window.change)
+                }
             }
             break
         case "RIGHT_ANALOG_STICK":
             if (stick.position.x < 0) {
                 window.guis.subArm5.setValue(window.guis.subArm5.object._y + (stick.position.x * -1) * 0.2)
+                if (window.record) {
+                    window.change = ["subArm5", (stick.position.x * -1) * 0.2]
+                    window.movement.push(window.change)
+                }
             } else {
                 window.guis.subArm5.setValue(window.guis.subArm5.object._y - (stick.position.x) * 0.2)
+                if (window.record) {
+                    window.change = ["subArm5", -Math.abs((stick.position.x * -1) * 0.2)]
+                    window.movement.push(window.change)
+                }
             }
             break
         default:
